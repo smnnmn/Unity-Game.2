429b4d042e8672da12febb643c5127377422ac2d
smnnmn <leejs899797@gmail.com>
2025-04-18 19:45:03 +0900

    {2025.4.18} 유저 인터페이스

---
+++ Game/Assets/Class7th User Interface/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Execute");
+    }
+    public void Shop()
+    {
+        Debug.Log("Shop");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
