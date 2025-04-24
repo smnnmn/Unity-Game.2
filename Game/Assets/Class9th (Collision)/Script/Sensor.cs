c55c416faef3bab73c0571eb3503459a5f1c9ed1
smnnmn <leejs899797@gmail.com>
2025-04-24 19:44:30 +0900

    {2025.4.24} 게임 오브젝트 생성 시스템

---
+++ Game/Assets/Class9th (Collision)/Script/Sensor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // OnTriggerEnter : 물리적이지 않은 충돌을 했을 때
+        // 호출되는 이벤트 함수입니다.
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        // OnTriggerEnter : 물리적이지 않은 충돌을 했을 때
+        // 호출되는 이벤트 함수입니다.
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // OnTriggerEnter : 물리적이지 않은 충돌을 했을 때
+        // 호출되는 이벤트 함수입니다.
+    }
+
+}
