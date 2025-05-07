23a060717078d60a122e6091526672580ada5989
smnnmn <leejs899797@gmail.com>
2025-05-07 19:45:31 +0900

    {2025.5.7} 파티클 시스템

--- Game/Assets/Class12th (Raycast)/Scripts/Mouse.cs
+++ Game/Assets/Class12th (Raycast)/Scripts/Mouse.cs
@@ -6,14 +6,25 @@ public class Mouse : MonoBehaviour
 {
     [SerializeField] Ray ray;
     [SerializeField] RaycastHit rayCastHit;
+    [SerializeField] LayerMask layerMask;
 
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray,out rayCastHit,Mathf.Infinity))
+            if(Physics.Raycast(ray,out rayCastHit,Mathf.Infinity,layerMask))
             {
+                GameObject prefab = rayCastHit.collider.gameObject;
+
+                if(prefab.transform.GetChild(0).gameObject.activeSelf)
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(false);
+                }
+                else
+                {
+                    prefab.transform.GetChild(0).gameObject.SetActive(true);
+                }
                 Debug.Log(rayCastHit.collider.name);
             }
         }
